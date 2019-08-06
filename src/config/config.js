e66c67603c0507426bf9ce6cb15c98d4dd3fe67e
hartleyn <nhartley18@me.com>
2019-08-06 01:58:18 -0400

    Updating resource URL to HTTPS.

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const PRODUCTION_HOST = 'http://67.205.171.177';
+const PRODUCTION_HOST = 'https://api.squarechess.ga';
 //const DEVELOPMENT_HOST = 'http://localhost:8000';
 
 export const HOST = PRODUCTION_HOST;
