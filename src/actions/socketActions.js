d899f693e55b2ad72f792f79f1516c0c979751e3
hartleyn <nhartley18@me.com>
2019-08-06 01:40:17 -0400

    Updating resource URLs to HTTPS and WSS.

--- src/actions/socketActions.js
+++ src/actions/socketActions.js
@@ -6,7 +6,7 @@ import {
 
 export const connectToMatchWebSocket = (matchId, authToken) => dispatch => {
   //const socketAddress = `ws://127.0.0.1:8000/ws/chess/matches/lobby/${matchId}/?${authToken}`;
-  const socketAddress = `ws://67.205.171.177/ws/chess/matches/lobby/${matchId}/?${authToken}`;
+  const socketAddress = `wss://api.squarechess.ga/ws/chess/matches/lobby/${matchId}/?${authToken}`;
   const matchSocket = new WebSocket(socketAddress);
   matchSocket.onopen = (event) => console.log('connected');
   matchSocket.onclose = (event) => console.log('connection closed');
